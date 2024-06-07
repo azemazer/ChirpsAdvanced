a8927706c24975f2c6475ec305ef3856504f56a6
azemazer <a.laval16@laposte.net>
2024-06-07 11:38:23 +0200

    base tuto

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use App\Http\Controllers\ChirpController;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/chirps', [ChirpController::class, 'index'])->name('chirps.index');
+    Route::post('/chirps', [ChirpController::class, 'store'])->name('chirps.store');
+    Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit'])->name('chirps.edit');
+    Route::put('/chirps/{chirp}', [ChirpController::class, 'update'])->name('chirps.update');
+    Route::patch('/chirps/{chirp}', [ChirpController::class, 'update'])->name('chirps.update');
+    Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy'])->name('chirps.destroy');
+});
+
+require __DIR__.'/auth.php';
